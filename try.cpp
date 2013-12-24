109cc597b640cbe58cd344224db665884def5bdf
itair <itair@163.com>
2013-12-24 19:31:02 +0800

    第五章 p 156 5.9

---
+++ try.cpp
@@ -0,0 +1,18 @@
+//C++ Primer Plus ¿ÎºóÏ°Ìâ p63 3.7 
+// by itAir
+// Try to use Google C++ programing style
+#include "try.h"
+#include <iostream>
+using namespace std;
+void main(){
+
+
+
+
+
+
+
+
+  system("pause");
+
+}
---
+++ try.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//TODO 加入一些声明与其他库
+#include <tchar.h>
+
