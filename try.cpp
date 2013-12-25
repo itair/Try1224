ebead1b8b3985721fcfa564e60e3796a800b4100
itair <itair@163.com>
2013-12-25 21:38:28 +0800

    第一 第二题

--- try.cpp
+++ try.cpp
@@ -1,18 +1,40 @@
-//C++ Primer Plus �κ�ϰ�� p63 3.7 
+﻿//C++ Primer Plus 课后习题 p164 5.9 
 // by itAir
 // Try to use Google C++ programing style
 #include "try.h"
 #include <iostream>
+#include <array>
+
 using namespace std;
+
 void main(){
+	cout<< "C++_Primer_Plus"<< endl;
+	cout<< "    by itAir"<< endl;        
+	cout<< "Hello World!"<< endl;
+	cout<< "_________________________________\n\n";    
+	// 1
+	int int_small , int_large;
+  int sum(0);
+	cout<< "Enter two interger begin with smaller one first: "<< endl;
+	cin>> int_small>> int_large;
+	for (int i = int_small; i <= int_large; i++){
+    sum += i;
+	}
+  cout<< "The sum from "<< int_small<< " to "<< int_large<< " is "<< sum<< endl; 
+  // 循环起始位置
+  cout<< "_________________________________\n\n";    
+  //2 
+  const int kArSize = 16;
+  array<double, kArSize> factorials;
+  factorials.at(0) = factorials.at(1) = 1.0;
+  for (int i = 2; i < kArSize; i++) factorials.at(i) = i * factorials.at(i-1);
+  for (int i = 0; i < kArSize; i++) cout<< i << "!= "<< factorials.at(i) <<endl;
+  //
+  cout<< "_________________________________\n\n";    
 
 
 
 
-
-
-
-
-  system("pause");
+	system("pause");
 
 }
--- try.h
+++ try.h
@@ -1,5 +1,7 @@
-#pragma once
+#ifndef TRY1224_TRY1224_TRY_H
+#define TRY1224_TRY1224_TRY_H
 
 //TODO 加入一些声明与其他库
 #include <tchar.h>
 
+#endif //TRY1224_TRY1224_TRY_H
