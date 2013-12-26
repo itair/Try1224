80625a5c48b4e502793924e042c25b66c36fcead
itair <itair@163.com>
2013-12-26 17:50:06 +0800

    全了

--- try.cpp
+++ try.cpp
@@ -27,14 +27,14 @@ void main(){
   showtitle();
   longline(1);
   // 1
-  //int int_small , int_large;
-  //int sum(0);
-  //cout<< "Enter two interger begin with smaller one first: "<< endl;
-  //cin>> int_small>> int_large;
-  //for (int i = int_small; i <= int_large; i++){
-  //	sum += i;
-  //}
-  //cout<< "The sum from "<< int_small<< " to "<< int_large<< " is "<< sum<< endl; */
+  int int_small , int_large;
+  int sum(0);
+  cout<< "Enter two interger begin with smaller one first: "<< endl;
+  cin>> int_small>> int_large;
+  for (int i = int_small; i <= int_large; i++){
+    sum += i;
+  }
+  cout<< "The sum from "<< int_small<< " to "<< int_large<< " is "<< sum<< endl; 
   // 循环起始位置
   longline(2);    
   //2
@@ -45,12 +45,12 @@ void main(){
   	// array 用法 类似于 带安全检查的 数组
   longline(3);   
   	//3
-  //int sum3(0), temp;
-  //cout<< "Enter some interger end with 0. \n";
-  //while ( cin>> temp && temp != 0 ){
-  //  sum3 += temp;
-  //}
-  //cout<< "Sum is :"<< sum3 <<endl;
+  int sum3(0), temp;
+  cout<< "Enter some interger end with 0. \n";
+  while ( cin>> temp && temp != 0 ){
+    sum3 += temp;
+  }
+  cout<< "Sum is :"<< sum3 <<endl;
   // 循环输入cin用法 , 条件判断的初始值 注意  
   longline(4);//代替长线 inline 型
   //4
@@ -68,53 +68,54 @@ void main(){
   //
   longline(5);
   //5
-  //int sum(0);
-  //array<int, 12> sale_month;
-  //cout<< "Enter salement by months:\n";
-  //for (int i = 0; i < 12; i++)  {
-  //  cout<< month.at(i)<<": ";
-  //  cin>> sale_month.at(i);
-  //  sum += sale_month.at(i);
-  //}
-  //cout<< "The sum of sales: "<< sum<< endl;
+  int sum1(0);
+  array<int, 12> sale_month;
+  cout<< "Enter salement by months:\n";
+  for (int i = 0; i < 12; i++)  {
+    cout<< kMonth.at(i)<<": ";
+    cin>> sale_month.at(i);
+    sum1 += sale_month.at(i);
+  }
+  cout<< "The sum of sales: "<< sum1<< endl;
   // 循环用法
   longline(6);
   //6 
-  //int sum_all(0);
-  //array<int, 3> sum_in_year;
-  //array<array<int, kMonthsPerYear>, 3> sale_month_;
-  //sum_in_year.fill(0);
-  //for (int i = 0; i <= 2; i ++) {
-  //  cout << "Enter salement by month in year " << i+1 << endl;
-  //  for (int j = 0; j < 12; j++) {
-  //    cout << kMonth.at(j) << " : ";
-  //    scanf_s("%d", &sale_month_[i][j]);
-  //    sum_in_year.at(i) += sale_month_.at(i).at(j);
-  //  }
-  //  sum_all += sum_in_year.at(i);
-  //  cout << "Sum of year " << i+1 << " is " << sum_in_year.at(i) <<endl;
-  //}
-  //cout << "\nSum of all 3 years : " << sum_all <<endl;
+  int sum_all(0);
+  array<int, 3> sum_in_year;
+  array<array<int, kMonthsPerYear>, 3> sale_month_;
+  sum_in_year.fill(0);
+  cin.clear();
+  for (int i = 0; i <= 2; i ++) {
+    cout << "Enter salement by month in year " << i+1 << endl;
+    for (int j = 0; j < 12; j++) {
+      cout << kMonth.at(j) << " : ";
+      scanf_s("%d", &sale_month_[i][j]);
+      sum_in_year.at(i) += sale_month_.at(i).at(j);
+    }
+    sum_all += sum_in_year.at(i);
+    cout << "Sum of year " << i+1 << " is " << sum_in_year.at(i) <<endl;
+  }
+  cout << "\nSum of all 3 years : " << sum_all <<endl;
   // const 定义 不要放入. h中
   longline(7);
   //7
-   //int num_car;
-   //cout << "How many cars do you wish to catalog? ";
-   //cin >> num_car;
-   //Car* cars = new  Car[num_car] ;
-   //for (int i = 0; i < num_car; i++ ) {
-   //  cout << "Car #" << i << ":\n"
-   //       << "Please enter the make: ";
-   //  cin.get(); //流定向到下一行...准备getline
-   //  getline(cin, cars[i].company_name);
-   //  cout << "Please enter the year made: ";
-   //  cin >> cars[i].year;
-   //  }
-   //cout << "Here is your colleciton: \n";
-   //for (int i =0; i < num_car; i++) {
-   //  cout << cars[i].year << "  " << cars[i].company_name << endl;
-   //}
-   //delete [] cars;
+   int num_car;
+   cout << "How many cars do you wish to catalog? ";
+   cin >> num_car;
+   Car* cars = new  Car[num_car] ;
+   for (int i = 0; i < num_car; i++ ) {
+     cout << "Car #" << i << ":\n"
+          << "Please enter the make: ";
+     cin.get(); //流定向到下一行...准备getline
+     getline(cin, cars[i].company_name);
+     cout << "Please enter the year made: ";
+     cin >> cars[i].year;
+     }
+   cout << "Here is your colleciton: \n";
+   for (int i =0; i < num_car; i++) {
+     cout << cars[i].year << "  " << cars[i].company_name << endl;
+   }
+   delete [] cars;
    //混合流中string的 geiline技巧 new 与 delete 
   longline(8);
   //8
@@ -135,8 +136,17 @@ void main(){
     count1++;
   }
   cout << "Your entered a total of " << count1 << " words.\n";
-
-
-
+  longline(10);
+  //10
+  int line;
+  cout << "Enter a line number: " <<endl;
+  cin >> line;
+  for (int i = 1; i <= line; i++){
+    for (int j = 1 ; j <= line - i; j++)  cout << ".";
+    for (int j = 1; j <= i; j++) cout << "*";
+    cout << endl;
+  }
+  // 多重循环控制
+  longline();
   system("pause");
 }
